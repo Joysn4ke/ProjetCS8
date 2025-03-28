0afdfefb8def7c73bbf106c1a99b151bfe960c27
Joysn4ke <joysn4ke@gmail.com>
2025-03-28 10:19:27 +0100

    merge

--- main.c
+++ main.c
@@ -32,48 +32,6 @@ int main()
 
     Game *game = newGame();
     gameInitialisation(game);
-<<<<<<< HEAD
-    gamePrint(game);
-    
-    while(!fin)
-    {
-        car = getch();
-        setGridCellMap(getMapGame(game), 
-               getPosPlayerX(getPlayerGame(game)),
-               getPosPlayerY(getPlayerGame(game)),
-               ' ');
-        switch(car)
-        {
-            case 'l':   //l = leave
-                fin = 1;
-                break;
-            case 65:    //flèche du haut
-            case 'z':   //haut
-                if (getPosPlayerX(getPlayerGame(game)) > 0) setPosPlayerX(getPlayerGame(game), getPosPlayerX(getPlayerGame(game)) - 1);
-                break;
-            case 66:    //lèche du bas
-            case 's':   //bas
-                if (getPosPlayerX(getPlayerGame(game)) < LINE - 1) setPosPlayerX(getPlayerGame(game), getPosPlayerX(getPlayerGame(game)) + 1);
-                break;
-            case 67:    //flèche de droite
-            case 'd':   //droite
-                if (getPosPlayerY(getPlayerGame(game)) < COLUMN - 1) setPosPlayerY(getPlayerGame(game), getPosPlayerY(getPlayerGame(game)) + 1);
-                break;
-            case 68:    //flèche de gauche
-            case 'q':   //gauche
-                if (getPosPlayerY(getPlayerGame(game)) > 0) setPosPlayerY(getPlayerGame(game), getPosPlayerY(getPlayerGame(game)) - 1);
-                break;
-        }
-
-        system("clear");
-        gamePrint(game);
-
-        // printf("treasure X : %d\n", getPosTreasureX(getTreasureGame(game)));printf("treasure Y : %d\n", getPosTreasureY(getTreasureGame(game)));
-
-        // printf("player X : %d\n", getPosPlayerX(getPlayerGame(game)));printf("player Y : %d\n", getPosPlayerY(getPlayerGame(game)));
-    }
-  
-=======
 
     setGridCellMap(getMapGame(game),
                getPosPlayerX(getPlayerGame(game)),
@@ -157,7 +115,6 @@ int main()
         }
         //printf("toto");
     }
->>>>>>> 0798d272cb99d5ae78723bf723c4f33d3f278bbe
 
     freeGame(game);
     return 0;
