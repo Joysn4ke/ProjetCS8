ff994bfeb555921d82e2e82b731a948784fdaa81
Joysn4ke <joysn4ke@gmail.com>
2025-03-31 09:34:42 +0200

    suppression commentaires inutiles

--- Source/Common.c
+++ Source/Common.c
@@ -5,35 +5,6 @@
 
 #include "Common.h"
 
-//void generateUniqueCoordinates(int* x, int* y, int* usedX, int* usedY, int nbUsed) {
-// void generateUniqueCoordinates(int* x, int* y, int* usedX, int* usedY) {
-    
-//     int newX, newY;
-//     int isUnique;
-
-//     do {
-//         //Generate new random coordinates
-//         newX = rand() % (LINE);
-//         newY = rand() % (COLUMN);
-
-//         //Verify the unicity of the generates coordinates
-//         isUnique = 1;
-//         for (int i = 0; i < TOTAL_ENTITY ; i++) {
-//             //Debug
-//             //printf("usedX[i] %d\n", usedX[i]);
-//             //printf("usedY[i] %d\n", usedY[i]);
-//             if (newX == usedX[i] && newY == usedY[i]) {
-//                 isUnique = 0;
-//                 break;
-//             }
-//         }
-//     } while (!isUnique);
-
-//     //Change the value of the new coordinates
-//     *x = newX;
-//     *y = newY;
-// }
-
 void generateUniqueCoordinates(int* x, int* y, int* usedX, int* usedY, int usedCount) {
     int newX, newY;
     int isUnique;
