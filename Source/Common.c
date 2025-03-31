1d9a88d3ab48d60d54c2a4d45a4b188b61d66957
Joysn4ke <joysn4ke@gmail.com>
2025-03-31 09:31:47 +0200

    refactorisation génération aléatoires de coordonnées

--- Source/Common.c
+++ Source/Common.c
@@ -5,26 +5,59 @@
 
 #include "Common.h"
 
-void generateUniqueCoordinates(int* x, int* y, int* usedX, int* usedY, int nbUsed) {
+//void generateUniqueCoordinates(int* x, int* y, int* usedX, int* usedY, int nbUsed) {
+// void generateUniqueCoordinates(int* x, int* y, int* usedX, int* usedY) {
     
+//     int newX, newY;
+//     int isUnique;
+
+//     do {
+//         //Generate new random coordinates
+//         newX = rand() % (LINE);
+//         newY = rand() % (COLUMN);
+
+//         //Verify the unicity of the generates coordinates
+//         isUnique = 1;
+//         for (int i = 0; i < TOTAL_ENTITY ; i++) {
+//             //Debug
+//             //printf("usedX[i] %d\n", usedX[i]);
+//             //printf("usedY[i] %d\n", usedY[i]);
+//             if (newX == usedX[i] && newY == usedY[i]) {
+//                 isUnique = 0;
+//                 break;
+//             }
+//         }
+//     } while (!isUnique);
+
+//     //Change the value of the new coordinates
+//     *x = newX;
+//     *y = newY;
+// }
+
+void generateUniqueCoordinates(int* x, int* y, int* usedX, int* usedY, int usedCount) {
     int newX, newY;
     int isUnique;
+
     do {
-        //Generate new random coordinates
+        // Generate new random coordinates
         newX = rand() % (LINE);
         newY = rand() % (COLUMN);
 
-        //Verify the unicity of the generates coordinates
-        isUnique = 0;
-        for (int i = 0; i < nbUsed; i++) {
+        // Verify the uniqueness of the generated coordinates
+        isUnique = 1;
+        for (int i = 0; i < usedCount; i++) {
             if (newX == usedX[i] && newY == usedY[i]) {
-                isUnique = 1;
+                isUnique = 0;
                 break;
             }
         }
-    } while (isUnique);
+    } while (!isUnique);
 
-    //Change the value of the new coordinates
+    // Change the value of the new coordinates
     *x = newX;
     *y = newY;
+    
+    // Store the new coordinates in the used arrays
+    usedX[usedCount] = newX;
+    usedY[usedCount] = newY;
 }
