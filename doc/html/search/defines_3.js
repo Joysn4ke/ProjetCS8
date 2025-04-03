fcfeaf2acbfe170c1d6c23f74f1e2626440cb622
Joysn4ke <joysn4ke@gmail.com>
2025-04-03 13:18:59 +0200

    MaJ doc

--- doc/html/search/defines_3.js
+++ doc/html/search/defines_3.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['nbpirate_0',['NBPIRATE',['../Common_8h.html#acfefcb2c1d4b2fae66b775b72c0aad1d',1,'Common.h']]],
   ['nbplayer_1',['NBPLAYER',['../Common_8h.html#a2a09cab61df2db96e5bdb1ae47ef7e7c',1,'Common.h']]],
-  ['nbtrap_2',['NBTRAP',['../Common_8h.html#a2ac687a9c2f7fce8313c25683756032d',1,'Common.h']]]
+  ['nbtrap_2',['NBTRAP',['../Common_8h.html#a2ac687a9c2f7fce8313c25683756032d',1,'Common.h']]],
+  ['nbtreasure_3',['NBTREASURE',['../Common_8h.html#ae552b0b3717ea7743881bc740a07639e',1,'Common.h']]]
 ];
